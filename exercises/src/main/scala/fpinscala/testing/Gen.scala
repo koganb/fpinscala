8339ce801e06677cc407df4f077572e6c1dfbd40
Boris Kogan <boris.kogan@cellebrite.com>
2015-02-26 11:13:19 +0200

    commit

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -22,11 +22,29 @@ case class Gen[A](sample: MyState[RNG, A]) {
         f(res._1).sample.run(res._2)
     }))
 
-    def listOfN(size: Gen[Int]): Gen[List[A]] = ???
+    def listOfN(size: Gen[Int]): Gen[List[A]] = Gen(MyState(s => {
+        val res: (Int, RNG) = size.sample.run(s)
+        val gen: Gen[List[A]] = Gen.listOfN(res._1, this)
+        gen.sample.run(res._2)
+
+
+    }))
 }
 
 
 object Gen {
+    def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = Gen(MyState(s => {
+        val res: (Boolean, RNG) = Gen.boolean.sample.run(s)
+        if (res._1) g1.sample.run(res._2)
+        else g2.sample.run(res._2)
+    }))
+
+
+    def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+        Gen(MyState(RNG.double)).flatMap[A]((a: Double) => if (a < g1._2 / (g1._2 + g2._2)) g1._1 else g2._1)
+    }
+
+
     def unit[A](a: => A): Gen[A] = Gen(MyState(RNG => (a, RNG)))
 
     def boolean: Gen[Boolean] = Gen(MyState(RNG.nonNegativeInt).map(n => n % 2 == 0))
@@ -34,9 +52,9 @@ object Gen {
 
     def choose(start: Int, stopExclusive: Int): Gen[Int] = {
         Gen(MyState(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
-
-
     }
+
+    def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = ???
 }
 
 //
